dbf27a7b6770399e51ea4d89761c59ef385d1b61
LeeDoyeon98 <dyl1229@naver.com>
2021-05-29 05:15:20 +0900

    ADD comments

--- VGGNet/model.py
+++ VGGNet/model.py
@@ -3,73 +3,98 @@ from torch import nn
 from torch.nn import functional as F
 import pytorch_lightning as pl
 from pytorch_lightning.metrics import functional as FM
+# 라이트닝을 적용하면 변수들의 디바이스 위치를 일일이 변경하지 않아도 되고,
+# 학습 시에 forward/backward/optimize 과정을 더 간단하게 작업할 수 있다.
+
+
+# hidden state에 쓰레기값이 저장되지 않도록 가중치를 초기화
+# https://stackoverflow.com/questions/49433936/how-to-initialize-weights-in-pytorch
+def init_weights(m):
+    if type(m) == nn.Linear:
+        torch.nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
+
 
 class VGGNet(pl.LightningModule):
+    # num_layers=16/19, input_size=3(RGB channel#), output_size=64
+    # num_classes=3(cheetah, leopard, jaguar), learning_rate = lr
     def __init__(self, num_layers, input_size, output_size, num_classes, learning_rate):
         super(VGGNet, self).__init__()
         self.num_classes = num_classes
         self.num_layer = num_layers
-        if num_layers == 16:
+        if num_layers == 16:  # VGG16의 conv층 개수
             self.layer_list = [2, 2, 3, 3, 3]  # +fc 3개
-        elif num_layers == 19:
+        elif num_layers == 19:  # VGG19의 conv층 개수
             self.layer_list = [2, 2, 4, 4, 4]  # +fc 3개
 
         self.input_size = input_size
         self.output_size = output_size
         self.lr = learning_rate
-        self.loss = F.cross_entropy
-        
-        # conv층 만들기
+        self.loss = nn.CrossEntropyLoss()  # cross entropy 손실 함수 사용
+        # convolution layer list
         conv_list = []
 
+        # 16/19 모델별 필요한 conv층 생성
         for i in range(5):
             conv_list.append(self.make_block(self.input_size, self.output_size, self.layer_list[i]))
             self.input_size = self.output_size
             if i < 3: self.output_size *= 2
 
         self.conv_layer = nn.Sequential(*conv_list)
+        # https://stackoverflow.com/questions/49433936/how-to-initialize-weights-in-pytorch
+        self.conv_layer.apply(init_weights)  # init weights
 
+        # fully connected layer
         fc_layer_hidden = 1024
         self.fc_layer = nn.Sequential(
-            nn.Linear(self.output_size * 7 * 7, fc_layer_hidden * 2),
+            nn.Linear(self.output_size * 7 * 7, fc_layer_hidden * 2),  # 1차원 벡터로 flatten
             nn.ReLU(inplace=True),
             nn.Linear(fc_layer_hidden * 2, fc_layer_hidden),
             nn.ReLU(inplace=True),
             nn.Linear(fc_layer_hidden, num_classes)
         )
+        self.fc_layer.apply(init_weigths)  # 가중치 초기화
 
+    # conv layer 생성 함수
     def make_block(self, input_size, output_size, num_loop):
+        # conv 층 하나마다 relu 함수를 적용한다. inplace=True로 따로 할당하지 않고 그 값에 바로 적용한다.
+        # 3x3 filter를 padding=1 stride=1로 적용한다. stride는 default값이 1이다.
         block_list = [nn.Conv2d(input_size, output_size, 3, padding=1), nn.ReLU(inplace=True)]
         for i in range(num_loop - 1):
             block_list.append(nn.Conv2d(output_size, output_size, 3, padding=1))
             block_list.append(nn.ReLU(inplace=True))
 
-        block_list.append(nn.MaxPool2d(2, 2))
+        block_list.append(nn.MaxPool2d(2, 2))  # 마지막에는 2x2 stride=2 max pooling을 적용한다.
         return nn.Sequential(*block_list)
 
+    # forward
     def forward(self, x):
         x = self.conv_layer(x)
-        x = x.view(-1, 512 * 7 * 7)
+        x = x.view(-1, 512 * 7 * 7)  # flatten
         x = self.fc_layer(x)
         return x
 
+    # training step으로 학습 자동화
     def training_step(self, batch, batch_nb):
-        x, y = batch
-        y_hat = self(x)
-        y_hat = F.softmax(y_hat, dim=1)
-        loss = self.loss(y_hat, y)
+        x, y = batch  # y는 label값
+        y_hat = self(x)  # y_hat은 해당 모델로 예상한 결과값
+        y_hat = F.softmax(y_hat, dim=1)  # 3개 이상의 분류 모델에는 softmax가 좋다
+        loss = self.loss(y_hat, y)  # 정해진 label과 prediction값 비교
         return loss
 
+    # https://www.secmem.org/blog/2021/01/07/pytorch-lightning-tutorial/
+    # epoch마다 validation loss 확인
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
-        y_hat = F.softmax(y_hatd, dim=1)
+        y_hat = F.softmax(y_hatd, dim=1)  # dim=1로 두번째 차원에 적용
         loss = self.loss(y_hat, y)
-        acc = FM.accuracy(y_hat, y)
+        acc = FM.accuracy(y_hat, y)  # 정확도 측정
 
         metrics = {'val_acc': acc, 'val_loss': loss}
-        self.log_dict(metrics)
+        self.log_dict(metrics)  # log_dict로 key: value의 딕셔너리 저장
 
+    # train과 별개의 test data로 정확도 측정
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
@@ -80,5 +105,7 @@ class VGGNet(pl.LightningModule):
         metrics = {'test_acc': acc, 'test_loss': loss}
         self.log_dict(metrics)
 
+    # SGD 대신 AdamW 사용(loss값 변화 증가)
     def configure_optimizers(self):
+        # weight decay 은 가중치 감쇠값. default 는 0이다.
         return torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=0.0001)
--- VGGNet/without_lightning.py
+++ VGGNet/without_lightning.py
@@ -1,20 +1,12 @@
-import torch
+import torch, gc
 import torch.nn as nn
-import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import math
-
-# 디바이스 설정
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-classes = ('leopard', 'jaguar', 'cheetah')
-transform = get_preprocess_function()
-
-# 하이퍼 파라미터
-num_epochs = 2
-learning_rate = 0.001
-batch_size = 120
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+# writer = SummaryWriter("./drive/MyDrive/runs/test")  # for tensorboard
 
 
 # 이미지 전처리
@@ -27,6 +19,21 @@ def get_preprocess_function():
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
     return preprocess
+
+
+# 디바이스 설정. 연산에 사용되는 값들이 같은 디바이스에 위치해야 한다.
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# 비교를 위해 seed값 고정
+torch.manual_seed(1234)
+torch.cuda.manual_seed_all(4567)
+
+# 하이퍼 파라미터
+classes = ('leopard', 'jaguar', 'cheetah')
+transform = get_preprocess_function()
+num_epochs = 20
+learning_rate = 1e-4
+batch_size = 120
 
 
 # train dataset 경로 설정
@@ -65,11 +72,13 @@ class VGGNet(nn.Module):
 
         # fully connected layer
         self.fc_layer = nn.Sequential(
-            nn.Linear(self.output_size * 7 * 7, 4096),  # flatten
+            nn.Linear(self.output_size * 7 * 7, 4096),  # 1차원 벡터로 flatten
             nn.ReLU(inplace=True),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes)
+            # torch.nn.CrossEntropyLoss() criterion이 LogSoftMax와 NULLoss를 함께 포함하므로 마지막 softmax 생략
+            # https://pytorch.org/docs/stable/generated/torch.nn.CrossEntropyLoss.html
         )
 
         # conv layer 하나당 relu 함수 적용, 마지막에 max pooling 적용
@@ -79,45 +88,80 @@ class VGGNet(nn.Module):
                 block_list.append(nn.Conv2d(output_size, output_size, 3, padding=1))
                 block_list.append(nn.ReLU(inplace=True))
 
-            block_list.append(nn.MaxPool2d(2, 2))
+            block_list.append(nn.MaxPool2d(2, 2))  # 2x2 stride=2 max pooling
             return nn.Sequential(*block_list)
 
     # forward 연산
+    def forward(self, x):
+        x = self.conv_layer(x)
+        x = x.view(-1, 512 * 7 * 7)  # flatten
+        x = self.fc_layer(x)
+        return x
 
 
-def forward(self, x):
-    x = self.conv_layer(x)
-    x = x.view(-1, 512 * 7 * 7)
-    x = self.fc_layer(x)
-    return x
-
-
-model = VGGNet(16, 3, 64, 3).to(device)  # VGG16 모델 생성
+model = VGGNet(16, 3, 64, 3).to(device)  # VGG16 모델 생성, device 위치로 이동
 # model = VGGNet(19, 3, 64, 3).to(device) # VGG 19 모델 생성
-criterion = nn.CrossEntropyLoss()  # 손실 함수
+criterion = nn.CrossEntropyLoss()  # 손실 함수 cross entropy 사용
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  # 경사하강법 최적화
 n_total_steps = len(train_loader)
 
-# 학습
+# epoch만큼 반복하며 학습
 for epoch in range(num_epochs):
-    for i, (inputs, labels) in enumerate(train_loader):
-        inputs = inputs.to(device)
+    # batch size만큼 반복한다
+    for i, (inputs, labels) in enumerate(train_loader):  # index, input data, label
+        inputs = inputs.to(device)  # cpu/gpu로 이동
         labels = labels.to(device)
 
         # forward
         outputs = model(inputs)
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs, labels)  # 손실값 계산
 
         # backward
         loss.backward()
 
         # 최적화
         optimizer.step()
-        optimizer.zero_grad()
+        optimizer.zero_grad()  # 값이 누적되므로 초기화
 
-        if (i + 1) % 100 == 0:
+        # 10번째마다 epoch, step, loss값 출력
+        if (i + 1) % 10 == 0:
             print(f'epoch {epoch + 1} / {num_epochs}, step {i + 1} / {n_total_steps}, loss = {loss.item():.4f}')
 
-            # 불필요한 CUDA 용량 확보
-            gc.collect()
+        # 불필요하게 할당된 CUDA 용량 확보
+        gc.collect()
         torch.cuda.empty_cache()
+
+# print("Training Finish")
+
+# save model
+MODEL_PATH = './drive/MyDrive/Data/Model/VGG16.pt'  # VGG16의 경우
+torch.save(model.state_dict(), MODEL_PATH)
+
+# test data load
+test_dataset = ImageFolder(root='./drive/MyDrive/Data/TestData', transform=transform)
+# test data는 class별로 100장이므로 batch_size=100, 클래스별 결과를 출력하기 위해서 shuffle=False
+test_loader = DataLoader(test_dataset, batch_size=100, shuffle=False)
+
+# 저장된 model load
+loaded_model = VGGNet(16, 3, 64, 3).to(device)
+loaded_model.load_state_dict(torch.load(MODEL_PATH))
+loaded_model.eval()  # 평가하는 과정에서 모든 노드를 사용하겠다는 의미
+
+with torch.no_grad():  # test 과정은 가중치에 영향을 주지 않아야 한다
+    total_acc = 0  # 종합 정확도
+    for images, labels in test_loader:
+        n_correct = 0
+        n_samples = 0
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = loaded_model(images)  # load한 모델로 test data 연산 수행
+        _, predictions = torch.max(outputs, 1)  # max함수는 value, index 값 리턴
+        n_samples += labels.shape[0]  # 현재 batch에서 sample의 개수를 누적
+        n_correct += (predictions == labels).sum().item()  # 예측값과 label 같은 개수의 합
+
+        acc = 100.0 * n_correct / n_samples
+        total_acc += acc
+        print(f'{classes[labels[0]]} accuracy = {acc}')  # 클래스별 정확도 출력
+
+    print("total accuracy = ", total_acc / 3)  # 종합 정확도 출력
