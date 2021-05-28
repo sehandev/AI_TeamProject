f4a4db66cc6ae07a69bad47b3da897906bcfb8db
maylilyo <maylily_o@naver.com>
2021-05-28 18:39:43 +0900

    edit googlenet

--- GoogleNet/googlenet.py
+++ GoogleNet/googlenet.py
@@ -1,28 +1,16 @@
-import warnings
-from collections import namedtuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
-from torch.hub import load_state_dict_from_url
 import os
 
-from typing import Optional, Tuple, List, Callable, Any
 import pytorch_lightning as pl
 from pytorch_lightning.metrics import functional as FM
-
-__all__ = ['GoogLeNet', 'googlenet', "GoogLeNetOutputs", "_GoogLeNetOutputs"]
-
-
-GoogLeNetOutputs = namedtuple('GoogLeNetOutputs', ['logits', 'aux_logits2', 'aux_logits1'])
-GoogLeNetOutputs.__annotations__ = {'logits', 'aux_logits2', 'aux_logits1'}
-
-_GoogLeNetOutputs = GoogLeNetOutputs
 
 
 def googlenet(learning_rate):
     model = GoogLeNet(learning_rate)
-    model.load_state_dict(torch.load('./googlenet-1378be20.pth'), strict=False)
+    model.load_state_dict(torch.load('/home/maylily/project/studing/AI_TeamProject/GoogleNet/googlenet-1378be20.pth'), strict=False)
     model.aux_logits = False
 
     fc_weights = model.fc.weight.data  # pytorch tensor
@@ -63,35 +51,31 @@ class GoogLeNet(pl.LightningModule):
     def __init__(self, learning_rate) :
         super(GoogLeNet, self).__init__()
 
-        conv_block = BasicConv2d
-        inception_block = Inception
-        inception_aux_block = InceptionAux
-
         self.aux_logits = True
         num_classes = 1000
 
-        self.conv1 = conv_block(3, 64, kernel_size=7, stride=2, padding=3)
+        self.conv1 = BasicConv2d(3, 64, kernel_size=7, stride=2, padding=3)
         self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
-        self.conv2 = conv_block(64, 64, kernel_size=1)
-        self.conv3 = conv_block(64, 192, kernel_size=3, padding=1)
+        self.conv2 = BasicConv2d(64, 64, kernel_size=1)
+        self.conv3 = BasicConv2d(64, 192, kernel_size=3, padding=1)
         self.maxpool2 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
 
-        self.inception3a = inception_block(192, 64, 96, 128, 16, 32, 32)
-        self.inception3b = inception_block(256, 128, 128, 192, 32, 96, 64)
+        self.inception3a = Inception(192, 64, 96, 128, 16, 32, 32)
+        self.inception3b = Inception(256, 128, 128, 192, 32, 96, 64)
         self.maxpool3 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
 
-        self.inception4a = inception_block(480, 192, 96, 208, 16, 48, 64)
-        self.inception4b = inception_block(512, 160, 112, 224, 24, 64, 64)
-        self.inception4c = inception_block(512, 128, 128, 256, 24, 64, 64)
-        self.inception4d = inception_block(512, 112, 144, 288, 32, 64, 64)
-        self.inception4e = inception_block(528, 256, 160, 320, 32, 128, 128)
+        self.inception4a = Inception(480, 192, 96, 208, 16, 48, 64)
+        self.inception4b = Inception(512, 160, 112, 224, 24, 64, 64)
+        self.inception4c = Inception(512, 128, 128, 256, 24, 64, 64)
+        self.inception4d = Inception(512, 112, 144, 288, 32, 64, 64)
+        self.inception4e = Inception(528, 256, 160, 320, 32, 128, 128)
         self.maxpool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-        self.inception5a = inception_block(832, 256, 160, 320, 32, 128, 128)
-        self.inception5b = inception_block(832, 384, 192, 384, 48, 128, 128)
+        self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
+        self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
 
-        self.aux1 = inception_aux_block(512, num_classes)
-        self.aux2 = inception_aux_block(528, num_classes)
+        self.aux1 = InceptionAux(512)
+        self.aux2 = InceptionAux(528)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.2)
@@ -101,15 +85,14 @@ class GoogLeNet(pl.LightningModule):
         self.lr = learning_rate
         self.loss = F.cross_entropy
 
-
-    def _transform_input(self, x) -> Tensor:
+    def _transform_input(self, x):
         x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
         x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
         x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
         x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
         return x
 
-    def _forward(self, x) -> Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
+    def _forward(self, x):
         # N x 3 x 224 x 224
         x = self.conv1(x)
         # N x 64 x 112 x 112
@@ -130,10 +113,10 @@ class GoogLeNet(pl.LightningModule):
         # N x 480 x 14 x 14
         x = self.inception4a(x)
         # N x 512 x 14 x 14
+
         aux1 = None
-        if self.aux1 is not None:
-            if self.training:
-                aux1 = self.aux1(x)
+        if self.training:
+            aux1 = self.aux1(x)
 
         x = self.inception4b(x)
         # N x 512 x 14 x 14
@@ -141,10 +124,10 @@ class GoogLeNet(pl.LightningModule):
         # N x 512 x 14 x 14
         x = self.inception4d(x)
         # N x 528 x 14 x 14
+
         aux2 = None
-        if self.aux2 is not None:
-            if self.training:
-                aux2 = self.aux2(x)
+        if self.training:
+            aux2 = self.aux2(x)
 
         x = self.inception4e(x)
         # N x 832 x 14 x 14
@@ -160,39 +143,27 @@ class GoogLeNet(pl.LightningModule):
         x = torch.flatten(x, 1)
         # N x 1024
         x = self.dropout(x)
-
-        #x = self.fc(x)
-        # N x 1000 (num_classes)
         x = self.fc3(x)
         # N x 3
         
         return x, aux2, aux1
 
-    @torch.jit.unused
-    def eager_outputs(self, x, aux2, aux1):
-        return x
-
     def forward(self, x):
         x = self._transform_input(x)
         x, aux1, aux2 = self._forward(x)
-        aux_defined = self.training and self.aux_logits
-        if torch.jit.is_scripting():
-            return GoogLeNetOutputs(x, aux2, aux1)
-        else:
-            return self.eager_outputs(x, aux2, aux1)
+        return x
     
     def training_step(self, batch, batch_nb):
         x, y = batch
         y_hat = self(x)
-        y_hat = F.softmax(y_hat, dim=1)
         loss = self.loss(y_hat, y)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
-        y_hat = F.softmax(y_hat, dim=1)
         loss = self.loss(y_hat, y)
+        y_hat = F.softmax(y_hat, dim=1)
         acc = FM.accuracy(y_hat, y)
 
         metrics = {'val_acc': acc, 'val_loss': loss}
@@ -201,8 +172,8 @@ class GoogLeNet(pl.LightningModule):
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        y_hat = F.softmax(y_hat, dim=1)
         loss = self.loss(y_hat, y)
+        y_hat = F.softmax(y_hat, dim=1)
         acc = FM.accuracy(y_hat, y)
 
         metrics = {'test_acc': acc, 'test_loss': loss}
@@ -214,61 +185,48 @@ class GoogLeNet(pl.LightningModule):
 class Inception(nn.Module):
     def __init__(
         self,
-        in_channels: int,
-        ch1x1: int,
-        ch3x3red: int,
-        ch3x3: int,
-        ch5x5red: int,
-        ch5x5: int,
-        pool_proj: int,
-        conv_block: Optional[Callable[..., nn.Module]] = None
-    ) -> None:
+        in_channels,
+        ch1x1,
+        ch3x3red,
+        ch3x3,
+        ch5x5red,
+        ch5x5,
+        pool_proj,
+    ):
         super(Inception, self).__init__()
-        if conv_block is None:
-            conv_block = BasicConv2d
-        self.branch1 = conv_block(in_channels, ch1x1, kernel_size=1)
+        self.branch1 = BasicConv2d(in_channels, ch1x1, kernel_size=1)
 
         self.branch2 = nn.Sequential(
-            conv_block(in_channels, ch3x3red, kernel_size=1),
-            conv_block(ch3x3red, ch3x3, kernel_size=3, padding=1)
+            BasicConv2d(in_channels, ch3x3red, kernel_size=1),
+            BasicConv2d(ch3x3red, ch3x3, kernel_size=3, padding=1)
         )
 
         self.branch3 = nn.Sequential(
-            conv_block(in_channels, ch5x5red, kernel_size=1),
-            conv_block(ch5x5red, ch5x5, kernel_size=3, padding=1)
+            BasicConv2d(in_channels, ch5x5red, kernel_size=1),
+            BasicConv2d(ch5x5red, ch5x5, kernel_size=3, padding=1)
         )
 
         self.branch4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1, ceil_mode=True),
-            conv_block(in_channels, pool_proj, kernel_size=1)
+            BasicConv2d(in_channels, pool_proj, kernel_size=1)
         )
 
-    def _forward(self, x) -> List[Tensor]:
+    def _forward(self, x):
         branch1 = self.branch1(x)
         branch2 = self.branch2(x)
         branch3 = self.branch3(x)
         branch4 = self.branch4(x)
-
         outputs = [branch1, branch2, branch3, branch4]
         return outputs
 
-    def forward(self, x) -> Tensor:
-        outputs = self._forward(x)
-        return torch.cat(outputs, 1)
+    def forward(self, x):
+        return torch.cat(self._forward(x), 1)
 
 
 class InceptionAux(nn.Module):
-
-    def __init__(
-        self,
-        in_channels: int,
-        num_classes: int,
-        conv_block: Optional[Callable[..., nn.Module]] = None
-    ) -> None:
+    def __init__(self, in_channels):
         super(InceptionAux, self).__init__()
-        if conv_block is None:
-            conv_block = BasicConv2d
-        self.conv = conv_block(in_channels, 128, kernel_size=1)
+        self.conv = BasicConv2d(in_channels, 128, kernel_size=1)
 
         self.fc1 = nn.Linear(2048, 1024)
         self.fc2 = nn.Linear(1024, 1000)  # original
@@ -285,28 +243,17 @@ class InceptionAux(nn.Module):
         x = F.relu(self.fc1(x), inplace=True)
         # N x 1024
         x = F.dropout(x, 0.7, training=self.training)
-        # N x 1024
-        #x = self.fc2(x)
-        # N x 1000 (num_classes)
-
-        x = self.fc3(x)
         # N x 3
+        x = self.fc3(x)
         return x
 
-
 class BasicConv2d(nn.Module):
-
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        **kwargs: Any
-    ) -> None:
+    def __init__(self, in_channels, out_channels, **kwargs):
         super(BasicConv2d, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
         self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
 
-    def forward(self, x) -> Tensor:
+    def forward(self, x):
         x = self.conv(x)
         x = self.bn(x)
         return F.relu(x, inplace=True)
