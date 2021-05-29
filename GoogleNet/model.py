3d57fa56a452371dba23f0aadcacb50eb3dc31c1
maylilyo <maylily_o@naver.com>
2021-05-29 19:53:38 +0900

    add function comment

--- GoogleNet/model.py
+++ GoogleNet/model.py
@@ -1,19 +1,18 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
-import os
-
 import pytorch_lightning as pl
 from pytorch_lightning.metrics import functional as FM
 
 
+
 class GoogLeNet(pl.LightningModule):
+    #GoogleNet(Inception v1) Class
     def __init__(self, learning_rate) :
         super(GoogLeNet, self).__init__()
-
-        self.aux_logits = True
-        num_classes = 1000
 
         self.conv1 = BasicConv2d(3, 64, kernel_size=7, stride=2, padding=3)
         self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
@@ -35,85 +34,69 @@ class GoogLeNet(pl.LightningModule):
         self.inception5a = Inception(832, 256, 160, 320, 32, 128, 128)
         self.inception5b = Inception(832, 384, 192, 384, 48, 128, 128)
 
-        self.aux1 = InceptionAux(512)
-        self.aux2 = InceptionAux(528)
+        self.aux1 = Aux(512)
+        self.aux2 = Aux(528)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.dropout = nn.Dropout(0.2)
-        self.fc = nn.Linear(1024, 1000)
         self.fc3 = nn.Linear(1024, 3)
 
         self.lr = learning_rate
         self.loss = F.cross_entropy
 
-    def _transform_input(self, x):
-        x_ch0 = torch.unsqueeze(x[:, 0], 1) * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
-        x_ch1 = torch.unsqueeze(x[:, 1], 1) * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
-        x_ch2 = torch.unsqueeze(x[:, 2], 1) * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
-        x = torch.cat((x_ch0, x_ch1, x_ch2), 1)
-        return x
-
-    def _forward(self, x):
-        # N x 3 x 224 x 224
+    def forward(self, x):
+        #The model has nine inception structure, one stem region, and three classifiers.
+        #Stem-region(not use inception-module area)
+        # In ( N x 3 x 224 x 224 ) ->
         x = self.conv1(x)
-        # N x 64 x 112 x 112
         x = self.maxpool1(x)
-        # N x 64 x 56 x 56
         x = self.conv2(x)
-        # N x 64 x 56 x 56
         x = self.conv3(x)
-        # N x 192 x 56 x 56
         x = self.maxpool2(x)
+        # -> Out ( N x 192 x 28 x 28 )
 
-        # N x 192 x 28 x 28
+        #The boundary of the inception area is divided by maxpool.
+        #inception area 1 (1, 2)
+        # In ( N x 192 x 28 x 28 ) ->
         x = self.inception3a(x)
-        # N x 256 x 28 x 28
         x = self.inception3b(x)
-        # N x 480 x 28 x 28
         x = self.maxpool3(x)
-        # N x 480 x 14 x 14
+        # -> Out ( N x 480 x 14 x 14 )
+
+        # inception area 2 (3 ~ 7)
+        # aux1, aux2 = auxiliary classifier 
+        # In ( N x 480 x 14 x 14 ) ->
         x = self.inception4a(x)
-        # N x 512 x 14 x 14
-
         aux1 = None
-        if self.training:
+        if self.training:  #aux1, aux2 is used only in trains (not in tests).
             aux1 = self.aux1(x)
-
         x = self.inception4b(x)
-        # N x 512 x 14 x 14
         x = self.inception4c(x)
-        # N x 512 x 14 x 14
         x = self.inception4d(x)
-        # N x 528 x 14 x 14
-
         aux2 = None
         if self.training:
             aux2 = self.aux2(x)
-
         x = self.inception4e(x)
-        # N x 832 x 14 x 14
         x = self.maxpool4(x)
-        # N x 832 x 7 x 7
-        x = self.inception5a(x)
-        # N x 832 x 7 x 7
-        x = self.inception5b(x)
-        # N x 1024 x 7 x 7
+        # -> Out ( N x 832 x 7 x 7 )
 
+        # inception area 3 (8, 9)
+        # In ( N x 832 x 7 x 7 ) ->
+        x = self.inception5a(x)
+        x = self.inception5b(x)
+        # -> Out ( N x 1024 x 7 x 7 )
+
+        #final Classifier
+        # In ( N x 1024 x 7 x 7 ) ->
         x = self.avgpool(x)
-        # N x 1024 x 1 x 1
         x = torch.flatten(x, 1)
-        # N x 1024
         x = self.dropout(x)
         x = self.fc3(x)
-        # N x 3
-        
-        return x, aux2, aux1
+        # -> Out ( N x 3 )
 
-    def forward(self, x):
-        x = self._transform_input(x)
-        x, aux1, aux2 = self._forward(x)
         return x
     
+    # obtains and passes the cross entropy loss between the output of the model and the correct label.
     def training_step(self, batch, batch_nb):
         x, y = batch
         y_hat = self(x)
@@ -121,16 +104,18 @@ class GoogLeNet(pl.LightningModule):
         loss = self.loss(y_hat, y)
         return loss
 
+    # obtains and stores model accuracy and cross entropy loss.
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
-        y_hat = F.softmax(y_hat, dim=1)
         loss = self.loss(y_hat, y)
-        acc = FM.accuracy(y_hat, y)
+        y_hat = F.softmax(y_hat, dim=1)
+        acc = FM.accuracy(y_hat, y)  # In logits, the maximum label matches the actual label.
 
         metrics = {'val_acc': acc, 'val_loss': loss}
         self.log_dict(metrics)
 
+    # records accuracy and cross entropy loss
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
@@ -145,71 +130,65 @@ class GoogLeNet(pl.LightningModule):
         return torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=0.0001)
 
 class Inception(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        ch1x1,
-        ch3x3red,
-        ch3x3,
-        ch5x5red,
-        ch5x5,
-        pool_proj,
-    ):
+    # Perform 1*1, 3*3, and 5*5 Convolution operations to efficiently extract features
+    def __init__( self, in_channels, ch1x1, ch3x3red, ch3x3, ch5x5red, ch5x5, pool_proj):
         super(Inception, self).__init__()
+        #1*1 Convolution operations
         self.branch1 = BasicConv2d(in_channels, ch1x1, kernel_size=1)
 
+        # Branch 2,3 use an additional 1*1 convolution layer. It has the effect of decreasing computation.
         self.branch2 = nn.Sequential(
-            BasicConv2d(in_channels, ch3x3red, kernel_size=1),
-            BasicConv2d(ch3x3red, ch3x3, kernel_size=3, padding=1)
+            BasicConv2d(in_channels, ch3x3red, kernel_size=1),      #1*1 Convolution operations
+            BasicConv2d(ch3x3red, ch3x3, kernel_size=3, padding=1)  #3*3 Convolution operations
         )
 
         self.branch3 = nn.Sequential(
-            BasicConv2d(in_channels, ch5x5red, kernel_size=1),
-            BasicConv2d(ch5x5red, ch5x5, kernel_size=3, padding=1)
+            BasicConv2d(in_channels, ch5x5red, kernel_size=1),      # 1*1 Convolution operations
+            BasicConv2d(ch5x5red, ch5x5, kernel_size=3, padding=1)  # 5*5 Convolution operations
         )
 
+        # Branch 4 use an additional 1*1 convolution laye after maxpool. 
+        # It is for reduce the number of channels.
         self.branch4 = nn.Sequential(
             nn.MaxPool2d(kernel_size=3, stride=1, padding=1, ceil_mode=True),
             BasicConv2d(in_channels, pool_proj, kernel_size=1)
         )
 
-    def _forward(self, x):
+    def forward(self, x):
         branch1 = self.branch1(x)
         branch2 = self.branch2(x)
         branch3 = self.branch3(x)
         branch4 = self.branch4(x)
-        outputs = [branch1, branch2, branch3, branch4]
-        return outputs
 
-    def forward(self, x):
-        return torch.cat(self._forward(x), 1)
+        # Change Dimensions
+        return torch.cat((branch1, branch2, branch3, branch4), 1)
 
 
-class InceptionAux(nn.Module):
+class Aux(nn.Module):
     def __init__(self, in_channels):
-        super(InceptionAux, self).__init__()
+        super(Aux, self).__init__()
+        # Googlenet uses an auxiliary classifier structure in two places 
+        # to solve the Vanishing gradient problem.
         self.conv = BasicConv2d(in_channels, 128, kernel_size=1)
-
         self.fc1 = nn.Linear(2048, 1024)
-        self.fc2 = nn.Linear(1024, 1000)  # original
-        self.fc3 = nn.Linear(1024, 3)  # copied
+        self.fc3 = nn.Linear(1024, 3)
 
     def forward(self, x):
-        # aux1: N x 512 x 14 x 14, aux2: N x 528 x 14 x 14
+        #The classifier from the third inception module is called Aux1, and the sixth is called Aux2.
+        
+        # Aux1 : In ( N x 512 x 14 x 14 ) ->
+        # Aux2 : In ( N x 528 x 14 x 14 ) ->
         x = F.adaptive_avg_pool2d(x, (4, 4))
-        # aux1: N x 512 x 4 x 4, aux2: N x 528 x 4 x 4
         x = self.conv(x)
-        # N x 128 x 4 x 4
         x = torch.flatten(x, 1)
-        # N x 2048
         x = F.relu(self.fc1(x), inplace=True)
-        # N x 1024
         x = F.dropout(x, 0.7, training=self.training)
-        # N x 3
         x = self.fc3(x)
+        # -> Out ( N x 3 )
         return x
 
 class BasicConv2d(nn.Module):
+    #Convolution layer class
     def __init__(self, in_channels, out_channels, **kwargs):
         super(BasicConv2d, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
